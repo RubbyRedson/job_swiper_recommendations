8432469c84bc7e9b7e1fc5528e2cc7854354e758
RubbyRedson <gurnik2009@gmail.com>
2016-12-02 22:23:30 +0100

    get job recs

--- src/main/java/se/openhack/jobsweeper/recommendation/controllers/MainController.java
+++ src/main/java/se/openhack/jobsweeper/recommendation/controllers/MainController.java
@@ -1,26 +1,44 @@
 package se.openhack.jobsweeper.recommendation.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import se.openhack.jobsweeper.recommendation.entities.JobRecommendation;
+import se.openhack.jobsweeper.recommendation.entities.JobRecommendationResponse;
+import se.openhack.jobsweeper.recommendation.entities.Tag;
 
-@Controller
+@RestController
 public class MainController {
 
     @RequestMapping(path = "/get_job_recs",
             method = RequestMethod.GET)
-    public @ResponseBody String getJobRecs(@RequestParam(value="userId") int id) {
+    public @ResponseBody JobRecommendationResponse getJobRecs(@RequestParam(value="userId") int id) {
+        Tag java = new Tag("Java");
+        Tag oop = new Tag("OOP");
+        Tag cSharp = new Tag("C#");
+        Tag programming = new Tag("Programming");
 
-        return "user/profile";
+        Tag[] firstTags = new Tag[2];
+        firstTags[0] = java;
+        firstTags[1] = oop;
+        JobRecommendation first = new JobRecommendation(1, firstTags);
+
+
+        Tag[] secTags = new Tag[2];
+        secTags[0] = cSharp;
+        secTags[1] = programming;
+        JobRecommendation second = new JobRecommendation(2, secTags);
+
+        JobRecommendation[] recs = new JobRecommendation[2];
+        recs[0] = first;
+        recs[1] = second;
+        JobRecommendationResponse response = new JobRecommendationResponse(recs);
+        return response;
     }
 
-    @RequestMapping(path = {"/user/update"},
-            method = RequestMethod.POST)
-    public Object updateUser(Model model) {
-        return "redirect:/metrics";
-
-    }
+//    @RequestMapping(path = {"/user/update"},
+//            method = RequestMethod.POST)
+//    public Object updateUser() {
+//        return "redirect:/metrics";
+//
+//    }
 }
