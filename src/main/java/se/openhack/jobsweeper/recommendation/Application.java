a7b62b11842765f3f3feb7cc330d14f135e06af4
RubbyRedson <gurnik2009@gmail.com>
2016-12-02 21:45:01 +0100

    Spring boot main class

---
+++ src/main/java/se/openhack/jobsweeper/recommendation/Application.java
@@ -0,0 +1,30 @@
+package se.openhack.jobsweeper.recommendation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
