d25e74940f7300433d8f7cc9ec60966de05e72ce
RubbyRedson <gurnik2009@gmail.com>
2016-12-03 08:00:22 +0100

    more mock data

--- src/main/java/se/openhack/jobsweeper/recommendation/database/DatabaseClient.java
+++ src/main/java/se/openhack/jobsweeper/recommendation/database/DatabaseClient.java
@@ -27,6 +27,10 @@ public class DatabaseClient {
         try (Session session = driver.session()) {
             session.run("CREATE (a:Job {title:'HR', id:'6965402'})");
             session.run("CREATE (a:Job {title:'Java Developer', id:'20662027'})");
+            session.run("CREATE (a:Job {title:'Java Tester Unix', id:'20674382'})");
+            session.run("CREATE (a:Job {title:'Web Developer', id:'20673861'})");
+            session.run("CREATE (a:Job {title:'Beauty Saloon Staff', id:'20674929'})");
+
 
             session.run("CREATE (a:User {name:'John', id:'1'})");
             session.run("CREATE (a:User {name:'Mike', id:'2'})");
@@ -42,11 +46,29 @@ public class DatabaseClient {
             session.run("CREATE (a:Tag {name:'Agile', id:'8'})");
             session.run("CREATE (a:Tag {name:'Docker', id:'9'})");
             session.run("CREATE (a:Tag {name:'Security', id:'10'})");
+            session.run("CREATE (a:Tag {name:'Linux', id:'11'})");
+            session.run("CREATE (a:Tag {name:'Testing', id:'12'})");
+            session.run("CREATE (a:Tag {name:'Web', id:'13'})");
+            session.run("CREATE (a:Tag {name:'HTML5', id:'14'})");
+            session.run("CREATE (a:Tag {name:'Javascript', id:'15'})");
+            session.run("CREATE (a:Tag {name:'Beauty', id:'16'})");
+            session.run("CREATE (a:Tag {name:'Make-up', id:'17'})");
 
 
             session.run("MATCH (a:Job { id:'6965402'}) " + " MATCH (b:Tag {id:'6'})" + "CREATE (a)-[c:has]->(b)");
             session.run("MATCH (a:Job { id:'6965402'}) " + " MATCH (b:Tag {id:'7'})" + "CREATE (a)-[c:has]->(b)");
             session.run("MATCH (a:Job { id:'6965402'}) " + " MATCH (b:Tag {id:'5'})" + "CREATE (a)-[c:has]->(b)");
+
+            session.run("MATCH (a:Job { id:'20674382'}) " + " MATCH (b:Tag {id:'1'})" + "CREATE (a)-[c:has]->(b)");
+            session.run("MATCH (a:Job { id:'20674382'}) " + " MATCH (b:Tag {id:'11'})" + "CREATE (a)-[c:has]->(b)");
+            session.run("MATCH (a:Job { id:'20674382'}) " + " MATCH (b:Tag {id:'12'})" + "CREATE (a)-[c:has]->(b)");
+
+            session.run("MATCH (a:Job { id:'20673861'}) " + " MATCH (b:Tag {id:'13'})" + "CREATE (a)-[c:has]->(b)");
+            session.run("MATCH (a:Job { id:'20673861'}) " + " MATCH (b:Tag {id:'14'})" + "CREATE (a)-[c:has]->(b)");
+            session.run("MATCH (a:Job { id:'20673861'}) " + " MATCH (b:Tag {id:'15'})" + "CREATE (a)-[c:has]->(b)");
+
+            session.run("MATCH (a:Job { id:'20674929'}) " + " MATCH (b:Tag {id:'16'})" + "CREATE (a)-[c:has]->(b)");
+            session.run("MATCH (a:Job { id:'20674929'}) " + " MATCH (b:Tag {id:'17'})" + "CREATE (a)-[c:has]->(b)");
 
             session.run("MATCH (a:Job { id:'20662027'}) " + " MATCH (b:Tag {id:'1'})" + "CREATE (a)-[c:has]->(b)");
             session.run("MATCH (a:Job { id:'20662027'}) " + " MATCH (b:Tag {id:'10'})" + "CREATE (a)-[c:has]->(b)");
@@ -58,10 +80,20 @@ public class DatabaseClient {
             session.run("MATCH (a:User { id:'1'})  MATCH (b:Tag {id:'5'}) CREATE (a)-[c:interested {counter:1}]->(b)");
             session.run("MATCH (a:User { id:'1'})  MATCH (b:Tag {id:'7'}) CREATE (a)-[c:interested {counter:1}]->(b)");
 
+            session.run("MATCH (a:User { id:'1'})  MATCH (b:Tag {id:'16'}) CREATE (a)-[c:interested {counter:1}]->(b)");
+            session.run("MATCH (a:User { id:'1'})  MATCH (b:Tag {id:'15'}) CREATE (a)-[c:interested {counter:1}]->(b)");
+
             session.run("MATCH (a:User { id:'2'}) " + " MATCH (b:Tag {id:'1'})" + "CREATE (a)-[c:interested {counter:1}]->(b)");
             session.run("MATCH (a:User { id:'2'}) " + " MATCH (b:Tag {id:'10'})" + "CREATE (a)-[c:interested {counter:1}]->(b)");
             session.run("MATCH (a:User { id:'2'}) " + " MATCH (b:Tag {id:'8'})" + "CREATE (a)-[c:interested {counter:1}]->(b)");
             session.run("MATCH (a:User { id:'2'}) " + " MATCH (b:Tag {id:'9'})" + "CREATE (a)-[c:interested {counter:1}]->(b)");
+
+            session.run("MATCH (a:User { id:'2'}) " + " MATCH (b:Tag {id:'11'})" + "CREATE (a)-[c:interested {counter:1}]->(b)");
+            session.run("MATCH (a:User { id:'2'}) " + " MATCH (b:Tag {id:'12'})" + "CREATE (a)-[c:interested {counter:1}]->(b)");
+
+            session.run("MATCH (a:User { id:'2'}) " + " MATCH (b:Tag {id:'13'})" + "CREATE (a)-[c:interested {counter:1}]->(b)");
+            session.run("MATCH (a:User { id:'2'}) " + " MATCH (b:Tag {id:'14'})" + "CREATE (a)-[c:interested {counter:1}]->(b)");
+            session.run("MATCH (a:User { id:'2'}) " + " MATCH (b:Tag {id:'15'})" + "CREATE (a)-[c:interested {counter:1}]->(b)");
         }
     }
 
@@ -73,7 +105,7 @@ public class DatabaseClient {
 
             HashMap<Integer, JobRecommendation> responseMap = new HashMap<>();
             int counter = recNumber;
-            while (result.hasNext() && counter > 0) {
+            while (result.hasNext()) {
                 Record record = result.next();
                 int jobId = Integer.parseInt(record.get("id").asString());
                 if (responseMap.containsKey(jobId)) {
@@ -82,10 +114,12 @@ public class DatabaseClient {
                     jobRecommendation.getTags().add(new Tag(record.get("tag").asString()));
                     responseMap.put(jobId, jobRecommendation);
                 } else {
-                    List<Tag> tags = new ArrayList<>();
-                    tags.add(new Tag(record.get("tag").asString()));
-                    responseMap.put(jobId, new JobRecommendation(jobId, tags));
-                    counter--;
+                    if (counter > 0) {
+                        List<Tag> tags = new ArrayList<>();
+                        tags.add(new Tag(record.get("tag").asString()));
+                        responseMap.put(jobId, new JobRecommendation(jobId, tags));
+                        counter--;
+                    }
                 }
                 System.out.println(record.get("id").asString() + " " + record.get("tag").asString());
             }
@@ -94,8 +128,7 @@ public class DatabaseClient {
             for (JobRecommendation jobRecommendation : responseMap.values()) {
                 jobRecommendations.add(jobRecommendation);
             }
-            JobRecommendationResponse response = new JobRecommendationResponse(jobRecommendations);
-            return response;
+            return new JobRecommendationResponse(jobRecommendations);
         }
     }
 
